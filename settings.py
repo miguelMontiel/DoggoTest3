0d3fe08715502a09f970841be7005d70b34d6aeb
miguelMontiel <miguelmontielgalicia@hotmail.com>
2018-03-29 18:01:05 -0500

    Add files via upload

--- settings.py
+++ settings.py
@@ -1,44 +1,80 @@
-import pygame
+import pygame as pg
 
-vec = pygame.math.Vector2
+vec = pg.math.Vector2
 
+# define some colors (R, G, B)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 DARKGREY = (40, 40, 40)
 LIGHTGREY = (100, 100, 100)
-RED = (255, 0, 0)
 GREEN = (0, 255, 0)
+RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
-BROWN = (186, 55, 5)
+BROWN = (106, 55, 5)
+CYAN = (0, 255, 255)
 
-WIDTH = 1024
-HEIGHT = 768
+# game settings
+WIDTH = 1024   # 16 * 64 or 32 * 32 or 64 * 16
+HEIGHT = 768  # 16 * 48 or 32 * 24 or 64 * 12
 FPS = 60
-TITLE = "Doggo tilemap demo"
+TITLE = "Tilemap Demo"
 BGCOLOR = BROWN
 
-TILESIZE = 32
-GRIDWITH = WIDTH / TILESIZE
+TILESIZE = 64
+GRIDWIDTH = WIDTH / TILESIZE
 GRIDHEIGHT = HEIGHT / TILESIZE
 
 WALL_IMG = 'tile_01.png'
 
-#Player settings
-PLAYER_SPEED = 250
-PLAYER_ROT_SPEED = 250
+# Player settings
+PLAYER_HEALTH = 100
+PLAYER_SPEED = 280
+PLAYER_ROT_SPEED = 200
 PLAYER_IMG = 'manBlue_gun.png'
-PLAYER_HIT_RECT = pygame.Rect(0, 0, 35, 35)
+PLAYER_HIT_RECT = pg.Rect(0, 0, 35, 35)
 BARREL_OFFSET = vec(30, 10)
 
-#Gun
+# Gun settings
 BULLET_IMG = 'bullet.png'
 BULLET_SPEED = 500
 BULLET_LIFETIME = 1000
 BULLET_RATE = 150
 KICKBACK = 200
-GUN_SPREAD = 3
+GUN_SPREAD = 5
+BULLET_DAMAGE = 10
 
-#Mobs
+# Mob settings
 MOB_IMG = 'zoimbie1_hold.png'
-MOB_SPEED = 150
-MOB_HIT_RECT = pygame.Rect(0, 0, 30, 30)
+MOB_SPEEDs = [150, 100, 50, 200, 500]
+MOB_HIT_RECT = pg.Rect(0, 0, 30, 30)
+MOB_HEALTH = 100
+MOB_DAMAGE = 10
+MOB_KNOCKBACK = 20
+AVOID_RADIUS = 50
+DETECT_RADIUS = 400
+
+MUZZLE_FLASHES = ['whitePuff15.png', 'whitePuff16.png', 'whitePuff17.png', 'whitePuff18.png']
+FLASH_DURATION = 40
+
+WALL_LAYER = 1
+PLAYER_LAYER = 2
+BULLET_LAYER = 3
+MOB_LAYER = 2
+EFFECTS_LAYER = 4
+ITEMS_LAYER = 1
+
+# Items
+ITEM_IMAGES = {'health': 'health_pack.png'}
+HEALTH_PACK_AMOUNT = 20
+BOB_RANGE = 20
+BOB_SPEED = .6
+
+# Sounds
+BG_MUSIC = 'espionage.ogg'
+PLAYER_HIT_SOUNDS = ['pain/8.wav', 'pain/9.wav', 'pain/10.wav', 'pain/11.wav']
+ZOMBIE_MOAN_SOUNDS = ['brains2.wav', 'brains3.wav', 'zombie-roar-1.wav', 'zombie-roar-2.wav',
+                      'zombie-roar-3.wav', 'zombie-roar-5.wav', 'zombie-roar-6.wav', 'zombie-roar-7.wav']
+ZOMBIE_HIT_SOUNDS = ['splat-15.wav']
+WEAPON_SOUNDS_GUN = ['sfx_weapon_singleshot2.wav']
+EFFECTS_SOUNDS = {'level_start': 'level_start.wav',
+                  'health_up': 'health_pack.wav'}
